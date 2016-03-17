d81b16980ab8549098635a8c610431893e35526b
Matthew Wang <matthewang.wang@gmail.com>
2016-03-16 22:30:59 -0400

    Revert "changes gamemode logic flow"
    This reverts commit 20bb98fb32cd242c49fa5043bc00b6bc23c76f12.

--- league/league.js
+++ league/league.js
@@ -19,32 +19,26 @@ else{
   var champ_jsonData = JSON.parse(champ_data);
 
   mattPlaying = true;
-  var mode = "Custom";
-  if (!("gameQueueConfigId" in jsonData)){
-    type = jsonData["gameQueueConfigId"];
-    if (type == 2){
-      mode = "Blind Pick";
-    }
-    else if (type == 14){
-      mode = "Normal Draft";
-    }
-    else if (type == 4){
-      mode = "Ranked";
-    }
-    else if (type == 41){
-      mode = "Ranked 3v3";
-    }
-    else if (type == 65){
-      mode = "ARAM";
-    }
-    else if (type == 31 || type == 32 || type == 33){
-      mode = "Bot";
-    }
-    else{
-      mode = "Unidentified";
-    }
+  var mode = "Custom/Unidentified";
+  type = jsonData["gameQueueConfigId"];
+  if (type == 2){
+    mode = "Blind Pick";
   }
-
+  else if (type == 14){
+    mode = "Normal Draft";
+  }
+  else if (type == 4){
+    mode = "Ranked";
+  }
+  else if (type == 41){
+    mode = "Ranked 3v3";
+  }
+  else if (type == 65){
+    mode = "ARAM";
+  }
+  else if (type == 31 || type == 32 || type == 33){
+    mode = "Bot";
+  }
   for (i = 0; i < jsonData["participants"].length; i++){
     if (jsonData['participants'][i]["summonerId"] == 65443758){
       var champid = jsonData['participants'][i]['championId'];
