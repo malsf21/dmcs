a409e17e4c6688c395acf5944eb566b475767649
malsf21 <matthewang.wang@gmail.com>
2016-04-21 14:57:58 -0400

    adds jquery

--- 2015-2016/php/database.php
+++ 2015-2016/php/database.php
@@ -123,6 +123,7 @@
 			?>
 		</div>
 	</div>
+	<script src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
 	<script src="../../resources/bootstrap-4.0.0-alpha.2/js/bootstrap.min.js"></script>
 	</body>
 </html>
