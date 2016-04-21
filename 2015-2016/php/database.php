174e39e24a6c301a68e76d0d48fddf74be10cfd0
malsf21 <matthewang.wang@gmail.com>
2016-04-21 14:48:24 -0400

    a bit more formatting

--- 2015-2016/php/database.php
+++ 2015-2016/php/database.php
@@ -9,92 +9,100 @@
 		<link href="../../resources/bootstrap-4.0.0-alpha.2/css/bootstrap.min.css" rel="stylesheet" />
 	</head>
 	<body>
-   	<form action="<?=$_SERVER['PHP_SELF']?>" method="post">
-    	Country: <input class="form-control" type="text" name="country">
-    	National animal: <input class="form-control" type="text" name="animal">
-    	<button class="btn btn-primary" type="submit" name="submit"></button>
-    </form>
-		<div class="card">
-	<?php
+		<div class="container">
+	   	<form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+				<div class="row">
+					<div class="col-md-6">
+			    	Country: <input class="form-control" type="text" name="country">
+					</div>
+					<div class="col-md-6">
+			    	National animal: <input class="form-control" type="text" name="animal">
+					</div>
+				</div>
+	    	<button class="btn btn-primary" type="submit" name="submit">Submit</button>
+	    </form>
+			<div class="card">
+			<?php
 
-		// set database server access variables:
-		$host = "localhost";
-		$user = "root";
-		$pass = "";
-		$db = "compsci";
+				// set database server access variables:
+				$host = "localhost";
+				$user = "root";
+				$pass = "";
+				$db = "compsci";
 
-		// open connection
-		$connection = mysql_connect($host, $user, $pass) or die ("Unable to connect!");
+				// open connection
+				$connection = mysql_connect($host, $user, $pass) or die ("Unable to connect!");
 
-		// select database
-		mysql_select_db($db) or die ("Unable to select database!");
+				// select database
+				mysql_select_db($db) or die ("Unable to select database!");
 
-		// create query
-		$query = "SELECT * FROM symbols";
+				// create query
+				$query = "SELECT * FROM symbols";
 
-		// execute query
-		$result = mysql_query($query) or die ("Error in query: $query. ".mysql_error());
+				// execute query
+				$result = mysql_query($query) or die ("Error in query: $query. ".mysql_error());
 
-		// see if any rows were returned
-		if (mysql_num_rows($result) > 0) {
+				// see if any rows were returned
+				if (mysql_num_rows($result) > 0) {
 
-    		// print them one after another
-    		echo "<table cellpadding=10 border=1>";
-    		while($row = mysql_fetch_row($result)) {
-        		echo "<tr>";
-				echo "<td>".$row[0]."</td>";
-        		echo "<td>" . $row[1]."</td>";
-        		echo "<td>".$row[2]."</td>";
-				echo "<td><a href=".$_SERVER['PHP_SELF']."?id=".$row[0].">Delete</a></td>";
-        		echo "</tr>";
-    		}
-		    echo "</table>";
+		    		// print them one after another
+		    		echo "<table cellpadding=10 border=1>";
+		    		while($row = mysql_fetch_row($result)) {
+		        		echo "<tr>";
+						echo "<td>".$row[0]."</td>";
+		        		echo "<td>" . $row[1]."</td>";
+		        		echo "<td>".$row[2]."</td>";
+						echo "<td><a href=".$_SERVER['PHP_SELF']."?id=".$row[0].">Delete</a></td>";
+		        		echo "</tr>";
+		    		}
+				    echo "</table>";
 
-		} else {
+				} else {
 
-    		// print status message
-    		echo "No rows found!";
-		}
+		    		// print status message
+		    		echo "No rows found!";
+				}
 
-		// free result set memory
-		mysql_free_result($result);
+				// free result set memory
+				mysql_free_result($result);
 
-		// set variable values to HTML form inputs
-		$country = mysql_escape_string($_POST['country']);
-    	$animal = mysql_escape_string($_POST['animal']);
+				// set variable values to HTML form inputs
+				$country = mysql_escape_string($_POST['country']);
+		    	$animal = mysql_escape_string($_POST['animal']);
 
-		// check to see if user has entered anything
-		if ($animal != "") {
-	 		// build SQL query
-			$query = "INSERT INTO symbols (country, animal) VALUES ('$country', '$animal')";
-			// run the query
-     		$result = mysql_query($query) or die ("Error in query: $query. ".mysql_error());
-			// refresh the page to show new update
-	 		echo "<meta http-equiv='refresh' content='0'>";
-		}
+				// check to see if user has entered anything
+				if ($animal != "") {
+			 		// build SQL query
+					$query = "INSERT INTO symbols (country, animal) VALUES ('$country', '$animal')";
+					// run the query
+		     		$result = mysql_query($query) or die ("Error in query: $query. ".mysql_error());
+					// refresh the page to show new update
+			 		echo "<meta http-equiv='refresh' content='0'>";
+				}
 
-		// if DELETE pressed, set an id, if id is set then delete it from DB
-		if (isset($_GET['id'])) {
+				// if DELETE pressed, set an id, if id is set then delete it from DB
+				if (isset($_GET['id'])) {
 
-			// create query to delete record
-			echo $_SERVER['PHP_SELF'];
-    		$query = "DELETE FROM symbols WHERE id = ".$_GET['id'];
+					// create query to delete record
+					echo $_SERVER['PHP_SELF'];
+		    		$query = "DELETE FROM symbols WHERE id = ".$_GET['id'];
 
-			// run the query
-     		$result = mysql_query($query) or die ("Error in query: $query. ".mysql_error());
+					// run the query
+		     		$result = mysql_query($query) or die ("Error in query: $query. ".mysql_error());
 
-			// reset the url to remove id $_GET variable
-			$location = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-			echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
-			exit;
+					// reset the url to remove id $_GET variable
+					$location = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+					echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
+					exit;
 
-		}
+				}
 
-		// close connection
-		mysql_close($connection);
+				// close connection
+				mysql_close($connection);
 
-	?>
-</div>
+			?>
+		</div>
+	</div>
 	<script src="../../resources/bootstrap-4.0.0-alpha.2/js/bootstrap.min.js"></script>
 	</body>
 </html>
