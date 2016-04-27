5bb6d715048e3f433d5ce89b87c987ceec1e48f7
malsf21 <matthewang.wang@gmail.com>
2016-04-27 15:11:10 -0400

    adds a home page

---
+++ 2015-2016/php/thebuzz/home.php
@@ -0,0 +1,37 @@
+<?php
+	require("functions/common.php");
+	if(empty($_SESSION['user'])){
+		header("Location: index.php");
+		die("Redirecting to index.php");
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="author" content="">
+		<title>Home | The Buzz</title>
+		<link href="../../../resources/bootstrap-4.0.0-alpha.2/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../../../resources/font-awesome-4.5.0/css/font-awesome.min.css" rel="stylesheet" />
+    <style>
+			.full-nav > li > .dropdown-menu { min-width: 300px;}
+    </style>
+	</head>
+	<body>
+		<?php include_once ('navbar.php'); ?>
+    <div class="container" style="padding-top:70px;">
+      <div class="row">
+        <div class="col-sm-8">
+          <h1 class="display-4">Hi <?php echo $_SESSION['user']['name'] ?>
+        </div>
+        <div class="col-sm-4">
+          <h4>Recent Buzzes</h4>
+        </div>
+      </div>
+    </div>
+    <script src="../../../resources/jquery/jquery2.min.js"></script>
+		<script src="../../../resources/bootstrap-4.0.0-alpha.2/js/bootstrap.min.js"></script>
+  </body>
+</html>
