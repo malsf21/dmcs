d756cc5cf28b574ba33c1cfe218caf133c23bbea
Matthew Wang <matthewang.wang@gmail.com>
2016-08-15 01:06:13 -0400

    adds twitch pulling code

---
+++ resources/twitch-pull/twitch-pull.js
@@ -0,0 +1,28 @@
+function httpGet(theUrl){
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+  xmlHttp.send( null );
+  return xmlHttp.responseText;
+}
+var streaming = false;
+var stream_game = "";
+function twitchFetch(twitchUser){
+  var stream_data = JSON.parse(httpGet("https://api.twitch.tv/kraken/streams/"+twitchUser+"?jsoncallback=?"));
+
+  if (stream_data["stream"] != null){
+    streaming = true;
+    stream_game = stream_data["stream"]["game"];
+  }
+  twitchInfo();
+}
+
+function betterTwitchFetch(twitchUser){
+  var stream_data = JSON.parse(httpGet("https://api.twitch.tv/kraken/streams/"+twitchUser+"?jsoncallback=?"));
+
+  if (stream_data["stream"] != null){
+    return(stream_data["stream"]["game"]);
+  }
+  else{
+    return false;
+  }
+}
